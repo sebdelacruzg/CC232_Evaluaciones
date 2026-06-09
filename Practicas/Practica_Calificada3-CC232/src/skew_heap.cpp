f2254d5615b24ca9dd1eaa7e11046a195d82ae96
sebastian <sebasdelacruzg@gmail.com>
2026-06-09 16:16:27 -0500

    feat: implementar lógica para encontrar candidatos de inserción en el Skew Heap

--- Practicas/Practica_Calificada3-CC232/src/skew_heap.cpp
+++ Practicas/Practica_Calificada3-CC232/src/skew_heap.cpp
@@ -4,7 +4,26 @@
 using namespace std;
 
 vector<int> encontrar_candidatos_insercion(const vector<NodoSkewHeap>& arbol, int raiz) {
-    return {};
+    if (raiz == 0) return {};
+    int nodo_columna_izquierda = 0;
+    int actual = raiz;
+    while (actual != 0) {
+        if (arbol[actual].derecho == 0) {
+            nodo_columna_izquierda = actual;
+            break;
+        }
+        actual = arbol[actual].izquierdo;
+    }
+    if (nodo_columna_izquierda == 0) return {};
+    vector<int> candidatos_validos;
+    candidatos_validos.push_back(nodo_columna_izquierda);
+    int candidato_hoja_izquierda = arbol[nodo_columna_izquierda].izquierdo;
+    if (candidato_hoja_izquierda != 0) {
+        if (arbol[candidato_hoja_izquierda].izquierdo == 0 && arbol[candidato_hoja_izquierda].derecho == 0) {
+            candidatos_validos.push_back(candidato_hoja_izquierda);
+        }
+    }
+    return candidatos_validos;
 }
 
 int revertir_insercion_skew_heap(vector<NodoSkewHeap>& arbol, int nodo_extraido, int raiz_actual) {
