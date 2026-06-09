835fd29b15c5e483f75ce67878f5cf4f87eb668b
sebastian <sebasdelacruzg@gmail.com>
2026-06-08 21:48:46 -0500

    feat: implementar funciones básicas para el manejo de NodoSkewHeap

---
+++ Practicas/Practica_Calificada3-CC232/src/skew_heap.cpp
@@ -0,0 +1,16 @@
+#include "skew_heap.h"
+#include <algorithm>
+
+using namespace std;
+
+vector<int> encontrar_candidatos_insercion(const vector<NodoSkewHeap>& arbol, int raiz) {
+    return {};
+}
+
+int revertir_insercion_skew_heap(vector<NodoSkewHeap>& arbol, int nodo_extraido, int raiz_actual) {
+    return 0;
+}
+
+vector<int> reconstruir_permutacion(const vector<NodoSkewHeap>& arbol_inicial, int numero_nodos, bool maximizar) {
+    return {};
+}
