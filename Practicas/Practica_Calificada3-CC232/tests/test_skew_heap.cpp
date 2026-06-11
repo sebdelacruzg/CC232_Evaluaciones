e94617f6f5811b6207228f77c5558616e66ab589
sebastian <sebasdelacruzg@gmail.com>
2026-06-10 20:43:37 -0500

    feat: agregar prueba unitaria para verificar la reconstrucción de permutaciones en el Skew Heap

--- Practicas/Practica_Calificada3-CC232/tests/test_skew_heap.cpp
+++ Practicas/Practica_Calificada3-CC232/tests/test_skew_heap.cpp
@@ -5,6 +5,25 @@
 
 using namespace std;
 
+void test_sample_1() {
+    int n = 7;
+    vector<NodoSkewHeap> arbol(n + 1);
+    // Sample Input 1
+    arbol[1].izquierdo = 2; arbol[1].derecho = 3; arbol[2].padre = 1; arbol[3].padre = 1;
+    arbol[2].izquierdo = 4; arbol[2].derecho = 5; arbol[4].padre = 2; arbol[5].padre = 2;
+    arbol[3].izquierdo = 6; arbol[3].derecho = 7; arbol[6].padre = 3; arbol[7].padre = 3;
+    
+    vector<int> min_p = reconstruir_permutacion(arbol, n, true);
+    vector<int> max_p = reconstruir_permutacion(arbol, n, false);
+    
+    vector<int> exp_min = {1, 3, 2, 7, 5, 6, 4};
+    vector<int> exp_max = {7, 1, 5, 3, 2, 6, 4};
+    
+    assert(min_p == exp_min);
+    assert(max_p == exp_max);
+    cout << "Sample 1 verificado.\n";
+}
+
 void test_invariante_columna_izquierda() {
     vector<NodoSkewHeap> arbol(5); 
     arbol[1].izquierdo = 2; arbol[2].padre = 1;
